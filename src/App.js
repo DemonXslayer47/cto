9712245b9f0788cf0be0bb3f1349d89dbaa7a9e9
sthota3 <sreehari.thota@slu.edu>
2023-12-01 20:28:01 -0600

    added files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './App.css';
+import CTO from './CTO';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CTO Tracking Form</h1>
+      <CTO />
+    </div>
+  );
+}
+
+export default App;
